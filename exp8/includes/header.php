cb026cfc22739c8450f609458b39c309d5d94112
rihaninamdar <68378126+rihaninamdar@users.noreply.github.com>
2021-01-02 13:22:10 +0530

    Add files via upload

---
+++ exp8/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                 <a class="navbar-brand" href="#" style="color:#fff; font-size:24px4px; line-height:25px; ">
+
+                  ONLINE COURSE REGISTRATION
+
+
+
+                </a>
+
+            </div>
+
+            
+            </div>
+        </div>
